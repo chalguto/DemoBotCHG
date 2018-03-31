7ac02b0d8074454ddef46460bcea66bf67e1d114
Christian Gutiérrez Torres <christian.gutierrez.torres@intercapit.com>
2018-03-31 20:39:19 -0300

    commit

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ server.post('/api/messages', connector.listen());
 
 /*----------------------------------------------------------------------------------------
  * Bot Storage: This is a great spot to register the private state storage for your bot. 
- * We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
+ * We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own! 
  * For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
  * ---------------------------------------------------------------------------------------- */
 
